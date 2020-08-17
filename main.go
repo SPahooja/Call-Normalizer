e3a2a08027ed68789b1bf9de077b7a47bfb8c18c
SPahooja <pahoojasahil@gmail.com>
2020-08-17 20:32:28 +0530

    Added TABLE and isert with err

--- main.go
+++ main.go
@@ -18,28 +18,55 @@ const (
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	err = resetDB(db, dbname)
-	if err != nil {
-		panic(err)
-	}
-	db.Close()
+	// db, err := sql.Open("postgres", psqlInfo)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// err = resetDB(db, dbname)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// db.Close()
 
 	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
-	db, err = sql.Open("postgres", psqlInfo)
+	db, err := sql.Open("postgres", psqlInfo)
 
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	err = db.Ping()
+
+	err = createTable(db)
 	if err != nil {
 		panic(err)
 	}
+	id, err := insertphone(db, "123456789")
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println("id=",id)
 }
+
+func insertphone(db *sql.DB, phone string) (int, error) {
+	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
+	var id int
+	err := db.QueryRow(statement, phone).Scan(&id)
+	if err != nil{
+		return -1 , err
+	}
+	return id, nil
+}
+
+func createTable(db *sql.DB) error {
+	statement := `
+	CREATE TABLE IF NOT EXISTS phone_numbers(
+		id SERIAL,
+		value VARCHAR(255)
+	)`
+	_, err := db.Exec(statement)
+	return err
+}
+
 func resetDB(db *sql.DB, name string) error {
 	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
 	if err != nil {
